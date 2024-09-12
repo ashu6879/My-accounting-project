314b5c5fa937d731211feed0ad508a29beadcd7c
ashu6879 <144018820+ashu6879@users.noreply.github.com>
2024-09-12 11:40:26 +0530

    add currency

--- vite/backend/controllers/invoiceController.js
+++ vite/backend/controllers/invoiceController.js
@@ -45,7 +45,7 @@ exports.addInvoice = async (req, res) => {
       projectID,
       invNum, // Set the auto-incremented invNum
       remarks,
-      currencyID,
+      currency,
       invDate: autoFilledInvDate, // Autofilled or provided invDate
       printdate: autoFilledPrintDate // Autofilled or provided printdate
     });
@@ -108,7 +108,7 @@ exports.getInvoices = async (req, res) => {
           remarks: 1,
           invDate: 1,
           printdate: 1,
-          currencyID: 1,
+          currency: 1,
           clientName: { $ifNull: ['$clientDetails.clientName', null] },
           clientEmail: { $ifNull: ['$clientDetails.clientEmail', null] },
           clientPhone: { $ifNull: ['$clientDetails.clientPhone', null] },
@@ -177,6 +177,7 @@ exports.getInvoiceById = async (req, res) => {
           remarks: 1,
           invDate: 1,
           printdate: 1,
+          currency :1,
           clientName: { $ifNull: ['$clientDetails.clientName', null] },
           clientEmail: { $ifNull: ['$clientDetails.clientEmail', null] },
           clientPhone: { $ifNull: ['$clientDetails.clientPhone', null] },
